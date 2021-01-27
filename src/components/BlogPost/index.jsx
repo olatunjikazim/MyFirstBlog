9b8adf9e41706f30df3b28eba4de2c6dc92c6942
olatunjikazim <reachkazeem200@gmail.com>
2021-01-27 15:18:25 +0100

    modified

--- src/components/BlogPost/index.jsx
+++ src/components/BlogPost/index.jsx
@@ -18,6 +18,35 @@ const BlogPost = (props) => {
             posted on July 21, 2016 by Sora Blogging Tips
           </span>
         </div>
+        <div>
+          <img
+            src="https://1.bp.blogspot.com/-uUyRrF_pZzI/V9_OnN0WMPI/AAAAAAAAD48/GDN9pK1mnuwJvYdO4SoCJ38adB0sMzmLQCLcB/s1600/Special-1.jpg"
+            alt="Post Image"
+            className="postImageContainer"
+          />
+        </div>
+
+        <div className="blogHeader">
+          <span className="blogCategory">Featured</span>
+          <h1 className="postTitle">Fitness Mantra To Live Fit Life</h1>
+          <span className="postedBy">
+            posted onJuly 21, 2016 bySora Blogging Tips
+          </span>
+          <span className="post1">
+            Midst first it, you're multiply divided. There don't, second his one
+            given the he one third rule fruit, very. Fill. Seed give firmament
+            doesn't land, isn't lesser creeping. Abundantly you called signs
+            waters yielding he cattle greater were evenin...
+          </span>
+        </div>
+        <div className="blogImage2">
+          <img
+            src="https://1.bp.blogspot.com/-q2G-P44QbXs/V9_PE1fnC-I/AAAAAAAAD5A/5zt-KGj_SpUZSQwI6DA5ldLYQfC3LvG8wCLcB/s1600/5.jpg"
+            alt="Post Image2"
+            srcset=""
+            className="postImage2"
+          />
+        </div>
       </Card>
     </div>
   );
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -8,7 +8,15 @@ import "./style.css";
  **/
 
 const SideBar = (props) => {
-  return <Card style={{ width: "27%", BorderRadius: "20px" }}>Sidebar</Card>;
+  return (
+    <Card style={{ width: "27%", BorderRadius: "20px" }}>
+      <img
+        src="https://3.bp.blogspot.com/-zlQO0zfYQd8/V9_M2m9Px-I/AAAAAAAAD4o/qucehvSfM-4210-UQCaFPr6hs7NvJdLHQCPcB/w680/2-1.jpg"
+        alt="side1"
+        className="sidePost1"
+      />
+    </Card>
+  );
 };
 
 export default SideBar;
